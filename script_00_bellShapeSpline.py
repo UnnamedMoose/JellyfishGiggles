6cec529881beaa9e424b0ab74d6e8e2a230055a6
Lidtke <artur.lidtke@gmail.com>
2023-08-12 11:18:38 +0200

    Kinematics, yay!

--- script_00_bellShapeSpline.py
+++ script_00_bellShapeSpline.py
@@ -12,6 +12,8 @@ import os
 import re
 import platform
 import matplotlib.patches as mpatches
+import scipy.interpolate
+import matplotlib.animation as animation
 
 font = {"family": "serif",
         "weight": "normal",
@@ -19,6 +21,20 @@ font = {"family": "serif",
 
 matplotlib.rc("font", **font)
 matplotlib.rcParams["figure.figsize"] = (9, 6)
+
+# %% Helper function.
+def niceFig(xlab=None, ylab=None, figsize=None):
+    fig, ax = plt.subplots(1, figsize=figsize)
+    ax.tick_params(axis='both', reset=False, which='both', length=5, width=2)
+    ax.tick_params(axis='y', direction='out', which="both")
+    ax.tick_params(axis='x', direction='out', which="both")
+    for spine in ['top', 'right','bottom','left']:
+        ax.spines[spine].set_linewidth(2)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.set_xlabel(xlab)
+    ax.set_ylabel(ylab)
+    return fig, ax
 
 # %% Jellyfish properties.
 # Create a function that approximates the shape.
@@ -216,6 +232,73 @@ np.savetxt("dataset_01_medusae/shape_Costello2020_snapshot3.txt", np.hstack([xy,
 plt.savefig("./outputs/bellShapeEvolution_Costello2020_data.png", dpi=200, bbox_inches="tight")
 
 # %% Figure out a way to describe each profile using the same set of control points.
+# This also has to be done using a single, continuous curve in a way that preserves volume.
+
+def profileFromParams(length, halfThickness, theta, s=np.linspace(0, 1, 101), ax=None, dxPlot=0, colour="b"):
+    """
+    Creates a jellyfish profile from a set of segment lengths, thicknesses and rotation angles.
+    Can also provide additional parameters to plot the profile on an external figure.
+
+    Parameters
+    ----------
+    length : TYPE
+        DESCRIPTION.
+    halfThickness : TYPE
+        DESCRIPTION.
+    theta : TYPE
+        DESCRIPTION.
+    s : TYPE, optional
+        DESCRIPTION. The default is np.linspace(0, 1, 101).
+    ax : TYPE, optional
+        DESCRIPTION. The default is None.
+    dxPlot : TYPE, optional
+        DESCRIPTION. The default is 0.
+    colour : TYPE, optional
+        DESCRIPTION. The default is "b".
+
+    Returns
+    -------
+    xy : TYPE
+        DESCRIPTION.
+    cps : TYPE
+        DESCRIPTION.
+    area : TYPE
+        DESCRIPTION.
+
+    """
+    cps_u, cps_l = [], []
+
+    xLast = np.array([-length[0]/2., -halfThickness[0]])
+    for i in range(len(halfThickness)):
+        xNew = xLast + [length[i], 0.]
+        xNew = rotatePoint(xNew, xLast, -theta[i])
+        xMid = (xLast + xNew) / 2.
+        vTan = (xNew - xLast) / np.linalg.norm(xNew - xLast)
+        vPer = np.array([-vTan[1], vTan[0]])
+
+        p0 = xMid + halfThickness[i]*vPer
+        p1 = xMid - halfThickness[i]*vPer
+        cps_u.append(p0)
+        cps_l.append(p1)
+
+        if ax is not None:
+            ax.plot([xLast[0]+dxPlot, xNew[0]+dxPlot], [xLast[1], xNew[1]], "ko-", lw=4, alpha=0.25)
+            ax.plot(xMid[0]+dxPlot, xMid[1], "rs")
+            ax.plot([p0[0]+dxPlot, p1[0]+dxPlot], [p0[1], p1[1]], "r--")
+
+        xLast = xNew
+
+    cps = np.vstack([cps_u, np.flipud(cps_l)])
+    xy = np.array([bspline(cps, u, d=2) for u in s])
+    area = polyArea(xy[:, 0], xy[:, 1])
+
+    if ax is not None:
+        ax.plot(cps[:, 0]+dxPlot, cps[:, 1], "ro")
+        ax.plot(xy[:, 0]+dxPlot, xy[:, 1], lw=2, c=colour)
+        ax.text(dxPlot, 0.05, "A={:.4f} units$^2$".format(area), va="bottom", ha="left")
+
+    return xy, cps, area
+
 
 halfThicknesses = np.array([
     [0.101, 0.099, 0.090],
@@ -269,137 +352,201 @@ for i in range(4):
 
 # Create the backbone and control points.
 for j in range(3):
-    cps_u, cps_l = [], []
-
-    xLast = np.array([-lengths[0, j]/2. + j*0.5, -halfThicknesses[0, j]])
-    for i in range(len(halfThicknesses)):
-        xNew = xLast + [lengths[i, j], 0.]
-        xNew = rotatePoint(xNew, xLast, -thetas[i,j ])
-        xMid = (xLast + xNew) / 2.
-        vTan = (xNew - xLast) / np.linalg.norm(xNew - xLast)
-        vPer = np.array([-vTan[1], vTan[0]])
-
-        p0 = xMid + halfThicknesses[i, j]*vPer
-        p1 = xMid - halfThicknesses[i, j]*vPer
-        cps_u.append(p0)
-        cps_l.append(p1)
-
-        ax.plot([xLast[0], xNew[0]], [xLast[1], xNew[1]], "ko-", lw=4, alpha=0.25)
-        ax.plot(xMid[0], xMid[1], "rs")
-        ax.plot([p0[0], p1[0]], [p0[1], p1[1]], "r--")
-
-        xLast = xNew
-
-    cps = np.vstack([cps_u, np.flipud(cps_l)])
-    xy = np.array([bspline(cps, u, d=2) for u in s])
-    area = polyArea(xy[:, 0], xy[:, 1])
-
-    ax.plot(cps[:, 0], cps[:, 1], "ro")
-    ax.plot(xy[:, 0], xy[:, 1], lw=2, c=colours[j])
-    ax.text(0.5*j, 0.05, "A={:.4f} units$^2$".format(area), va="bottom", ha="left")
+    xy, cps, area = profileFromParams(lengths[:, j], halfThicknesses[:, j], thetas[:, j],
+                                      s=np.linspace(0, 1, 101), ax=ax, dxPlot=0.5*j, colour=colours[j])
 
 plt.savefig("./outputs/bellShapeEvolution_Costello2020_kinematicsModel.png", dpi=200, bbox_inches="tight")
 
+# %% Create time series of the bell shape parameters for continuous motion.
+
+# Function for smoothing the sparse kinematics data. A lot of fidgeting involved
+# to make sure the curves are continuous when going from t=1 to t=0 and to avoid
+# substantial overshooting.
+def smoothWithSpline(tf, yf, t):
+    tf = [tf[0], tf[0]+0.01, tf[1], tf[2], tf[-1]-0.5, tf[-1]-0.4, tf[-1]-0.15, tf[-1]]
+    yf = [yf[0], yf[0], yf[1], yf[2], yf[-1], yf[-1], yf[-1], yf[-1]]
+    spl = scipy.interpolate.splrep(tf, yf, s=0., k=2)
+    return scipy.interpolate.splev(t, spl)
+
+# TODO digitise from Costello plot.
+timeVals = [0, 0.2, 0.4, 1.0]
+
+fig, axes = plt.subplots(nrows=3, sharex=True, figsize=(8, 12))
+axes[-1].set_xlabel("t/T")
+axes[0].set_ylabel("Length")
+axes[1].set_ylabel("Half-thickness")
+axes[2].set_ylabel("Angle")
+for ax in axes:
+    ax.xaxis.set_ticks(timeVals)
+plt.subplots_adjust(top=0.985, bottom=0.13, left=0.14, right=0.97, hspace=0.15, wspace=0.195)
+
+L = np.hstack([lengths, lengths[:, 0][:, np.newaxis]])
+thk = np.hstack([halfThicknesses, halfThicknesses[:, 0][:, np.newaxis]])
+theta = np.hstack([thetas, thetas[:, 0][:, np.newaxis]])
+colours = plt.cm.Greens(np.linspace(0.3, 1, L.shape[0]))
+for i in range(L.shape[0]):
+    t = np.linspace(0, 1, 101)
+    Lfit = smoothWithSpline(timeVals, L[i, :], t)
+    thkFit = smoothWithSpline(timeVals, thk[i, :], t)
+    thetaFit = smoothWithSpline(timeVals, theta[i, :], t)
+
+    axes[0].plot(timeVals, L[i, :]+i*0, "o--", c=colours[i], alpha=1)
+    axes[0].plot(t, Lfit, c=colours[i], lw=2)
+    axes[1].plot(timeVals, thk[i, :]+i*0, "o--", c=colours[i], alpha=1)
+    axes[1].plot(t, thkFit, c=colours[i], lw=2)
+    axes[2].plot(timeVals, theta[i, :]+i*0, "o--", c=colours[i], alpha=1)
+    axes[2].plot(t, thetaFit, c=colours[i], lw=2)
+
+plt.savefig("./outputs/bellShapeEvolution_Costello2020_kinematicsParams.png", dpi=200, bbox_inches="tight")
+
+# %% Animate the jellyfish!
+
+# Set up the plot
+fig, ax = niceFig(figsize=(8, 10))
+ax.set_ylabel("Bell height coordinate")
+ax.set_xlabel("Bell width coordinate")
+ax.set_xlim((0, 0.6))
+ax.set_ylim((-0.8, 0.2))
+ax.set_aspect("equal", "box")
+
+# TODO Plot constant elements
+
+class JellyfishPlot(object):
+    def __init__(self, T):#, arr_c, arr_v):
+        # self.cs = cs
+        self.T = T
+        self.plotObjects = None
+        # self.arr_c = arr_c
+        # self.arr_v = arr_v
+
+    def animate(self, i):
+        # for c in self.cs.collections:
+        #     c.remove()
+        if self.plotObjects is not None:
+            for c in self.plotObjects:
+                c.remove()
+
+        t = i/self.T % 1
+
+        Lfit, thkFit, thetaFit = [], [], []
+        for j in range(L.shape[0]):
+            Lfit.append(smoothWithSpline(timeVals, L[j, :], t))
+            thkFit.append(smoothWithSpline(timeVals, thk[j, :], t))
+            thetaFit.append(smoothWithSpline(timeVals, theta[j, :], t))
+
+        xy, cps, area = profileFromParams(Lfit, thkFit, thetaFit)
+        self.plotObjects = ax.plot(xy[:, 0], xy[:, 1], c="r", lw=2)
+        self.plotObjects += ax.plot(cps[:, 0], cps[:, 1], "ko--", lw=1, alpha=0.25)
+        ax.set_title("A={:.4f} units$^2$".format(area))
+
+        return self.plotObjects
+
+jellyfishPlot = JellyfishPlot(150)
+
+anim = animation.FuncAnimation(fig, jellyfishPlot.animate, repeat=False, frames=range(150))
+writer = animation.PillowWriter(fps=20)
+anim.save('./outputs/jellyfishAnimation.gif', writer=writer, dpi=200)
+
 # %% Interactive plot.
-print(a)
-fig, ax = plt.subplots(figsize=(9, 9))
-plt.subplots_adjust(bottom=0.3)
-ax.set_xlabel("Bell width")
-ax.set_ylabel("Bell height")
-ax.plot(xy[:, 0], xy[:, 1], "k--", lw=2)
-ax.plot(xy[:, 0], xy[:, 1]-t/2., "k-")
-ax.plot(xy[:, 0], xy[:, 1]+t/2., "k-")
+# print(a)
+# fig, ax = plt.subplots(figsize=(9, 9))
+# plt.subplots_adjust(bottom=0.3)
+# ax.set_xlabel("Bell width")
+# ax.set_ylabel("Bell height")
+# ax.plot(xy[:, 0], xy[:, 1], "k--", lw=2)
+# ax.plot(xy[:, 0], xy[:, 1]-t/2., "k-")
+# ax.plot(xy[:, 0], xy[:, 1]+t/2., "k-")
 
-colours = plt.cm.viridis(np.linspace(0, 1, len(refDeformedShape)))
-for i in range(len(refDeformedShape)):
-    ax.plot(refDeformedShape[i]["x"], refDeformedShape[i]["y"], "-", c=colours[i])
+# colours = plt.cm.viridis(np.linspace(0, 1, len(refDeformedShape)))
+# for i in range(len(refDeformedShape)):
+#     ax.plot(refDeformedShape[i]["x"], refDeformedShape[i]["y"], "-", c=colours[i])
 
-sldr_ax1 = fig.add_axes([0.15, 0.15, 0.7, 0.05])
-sldr1 = matplotlib.widgets.Slider(sldr_ax1, 'Var 1', 0, 1, valinit=0, valfmt="%.1f")
-sldr_ax2 = fig.add_axes([0.15, 0.08, 0.7, 0.05])
-sldr2 = matplotlib.widgets.Slider(sldr_ax2, 'Var 2', 0, 1, valinit=0, valfmt="%.1f")
-sldr_ax3 = fig.add_axes([0.15, 0.01, 0.7, 0.05])
-sldr3 = matplotlib.widgets.Slider(sldr_ax3, 'Var 3', 0, 1, valinit=0, valfmt="%.1f")
+# sldr_ax1 = fig.add_axes([0.15, 0.15, 0.7, 0.05])
+# sldr1 = matplotlib.widgets.Slider(sldr_ax1, 'Var 1', 0, 1, valinit=0, valfmt="%.1f")
+# sldr_ax2 = fig.add_axes([0.15, 0.08, 0.7, 0.05])
+# sldr2 = matplotlib.widgets.Slider(sldr_ax2, 'Var 2', 0, 1, valinit=0, valfmt="%.1f")
+# sldr_ax3 = fig.add_axes([0.15, 0.01, 0.7, 0.05])
+# sldr3 = matplotlib.widgets.Slider(sldr_ax3, 'Var 3', 0, 1, valinit=0, valfmt="%.1f")
 
-lns = None
-patches = None
-texts = None
+# lns = None
+# patches = None
+# texts = None
 
-def onChanged(val):
-    global lns, patches, texts
-    if lns is not None:
-        for c in lns:
-            c.remove()
-        for c in patches:
-            c.remove()
-        for c in texts:
-            c.remove()
+# def onChanged(val):
+#     global lns, patches, texts
+#     if lns is not None:
+#         for c in lns:
+#             c.remove()
+#         for c in patches:
+#             c.remove()
+#         for c in texts:
+#             c.remove()
 
-    # ---
+#     # ---
 
-    # Version 1 - rotate points of the bounding spline.
-    # => does not preserve volume.
-    cps0 = np.array([
-        [0, 0.12],
-        [0.35, 0.05],
-        [0.97, -0.55],
-        [1.0, -1.15],
-        [0.92, -0.6],
-        [0.3, -0.19],
-        [0, -0.12]
-    ])
+#     # Version 1 - rotate points of the bounding spline.
+#     # => does not preserve volume.
+#     cps0 = np.array([
+#         [0, 0.12],
+#         [0.35, 0.05],
+#         [0.97, -0.55],
+#         [1.0, -1.15],
+#         [0.92, -0.6],
+#         [0.3, -0.19],
+#         [0, -0.12]
+#     ])
 
-    # Apply 1st rotation
-    cps = np.copy(cps0)
-    cps[1, :] = rotatePoint(cps[1, :], cps[0, :], sldr1.val*5./180.*np.pi)
-    cps[-2, :] = rotatePoint(cps[-2, :], cps[-1, :], sldr1.val*5./180.*np.pi)
-    for i in range(2, cps.shape[0]-2):
-        cps[i, :] = rotatePoint(cps[i, :], [0, 0], sldr1.val*5./180.*np.pi)
+#     # Apply 1st rotation
+#     cps = np.copy(cps0)
+#     cps[1, :] = rotatePoint(cps[1, :], cps[0, :], sldr1.val*5./180.*np.pi)
+#     cps[-2, :] = rotatePoint(cps[-2, :], cps[-1, :], sldr1.val*5./180.*np.pi)
+#     for i in range(2, cps.shape[0]-2):
+#         cps[i, :] = rotatePoint(cps[i, :], [0, 0], sldr1.val*5./180.*np.pi)
 
-    # Apply 2nd rotation
-    cps[2, :] = rotatePoint(cps[2, :], cps[1, :], sldr2.val*20./180.*np.pi)
-    cps[-3, :] = rotatePoint(cps[-3, :], cps[-2, :], sldr2.val*20./180.*np.pi)
-    x0 = (cps[1, :] + cps[-2, :])/2.
-    for i in range(3, cps.shape[0]-3):
-        cps[i, :] = rotatePoint(cps[i, :], x0, sldr2.val*20./180.*np.pi)
+#     # Apply 2nd rotation
+#     cps[2, :] = rotatePoint(cps[2, :], cps[1, :], sldr2.val*20./180.*np.pi)
+#     cps[-3, :] = rotatePoint(cps[-3, :], cps[-2, :], sldr2.val*20./180.*np.pi)
+#     x0 = (cps[1, :] + cps[-2, :])/2.
+#     for i in range(3, cps.shape[0]-3):
+#         cps[i, :] = rotatePoint(cps[i, :], x0, sldr2.val*20./180.*np.pi)
 
-    # Rotate the tip.
-    x0 = (cps[2, :] + cps[4, :])/2.
-    cps[3, :] = rotatePoint(cps[3, :], x0, sldr3.val*90./180.*np.pi)
+#     # Rotate the tip.
+#     x0 = (cps[2, :] + cps[4, :])/2.
+#     cps[3, :] = rotatePoint(cps[3, :], x0, sldr3.val*90./180.*np.pi)
 
-    # Version 2 - work with a "backbone" spline and derive CP positions from that
-    #   based on local thickness and normal vectors. Scale the thickness iteratively
-    #   to enforce volume conservation to a particular threshold.
-    # =>
+#     # Version 2 - work with a "backbone" spline and derive CP positions from that
+#     #   based on local thickness and normal vectors. Scale the thickness iteratively
+#     #   to enforce volume conservation to a particular threshold.
+#     # =>
 
-    # TODO
+#     # TODO
 
-    # ---
+#     # ---
 
-    # Annotate CP segment lengths.
-    texts = []
-    for i in range(cps0.shape[0]-1):
-        x0 = np.sum(cps[i:i+2, :], axis=0) / 2.
-        ds = np.linalg.norm(cps[i+1, :] - cps[i, :])
-        texts.append(ax.text(x0[0], x0[1], "{:.3f}".format(ds), va="center", ha="center"))
+#     # Annotate CP segment lengths.
+#     texts = []
+#     for i in range(cps0.shape[0]-1):
+#         x0 = np.sum(cps[i:i+2, :], axis=0) / 2.
+#         ds = np.linalg.norm(cps[i+1, :] - cps[i, :])
+#         texts.append(ax.text(x0[0], x0[1], "{:.3f}".format(ds), va="center", ha="center"))
 
-    # Compute the spline and plot the outline.
-    s = np.linspace(0, 1, 101)
-    p = np.array([bspline(cps, u, d=2) for u in s])
-    lns = ax.plot(cps[:, 0], cps[:, 1], "o--", c="orange", ms=7)
-    lns += ax.plot(p[:, 0], p[:, 1], "m-")
+#     # Compute the spline and plot the outline.
+#     s = np.linspace(0, 1, 101)
+#     p = np.array([bspline(cps, u, d=2) for u in s])
+#     lns = ax.plot(cps[:, 0], cps[:, 1], "o--", c="orange", ms=7)
+#     lns += ax.plot(p[:, 0], p[:, 1], "m-")
 
-    # Fill-in the contour.
-    patches = [ax.add_patch(mpatches.Polygon(p[:, :2], facecolor="m", alpha=0.25))]
+#     # Fill-in the contour.
+#     patches = [ax.add_patch(mpatches.Polygon(p[:, :2], facecolor="m", alpha=0.25))]
 
-    # Compute cross-section area.
-    area = polyArea(p[:, 0], p[:, 1])
-    ax.set_title("Area = {:.4f} units$^2$".format(area))
+#     # Compute cross-section area.
+#     area = polyArea(p[:, 0], p[:, 1])
+#     ax.set_title("Area = {:.4f} units$^2$".format(area))
 
-    return lns
+#     return lns
 
-sldr1.on_changed(onChanged)
-sldr2.on_changed(onChanged)
-sldr3.on_changed(onChanged)
+# sldr1.on_changed(onChanged)
+# sldr2.on_changed(onChanged)
+# sldr3.on_changed(onChanged)
 
-lns = onChanged(0.)
+# lns = onChanged(0.)
